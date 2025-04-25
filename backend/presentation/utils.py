bcd8875f2a0cb6923b3e8e7ef9266c8def323230
Mohit Rana <mohitr8998@gmail.com>
2025-04-25 04:07:00 -0400

    added templates for the slides in backend

--- backend/presentation/utils.py
+++ backend/presentation/utils.py
@@ -12,25 +12,79 @@ You are an expert presentation creator. Generate a {number_of_slides}-slide pres
 
 \"{prompt}\"
 
-Return the output strictly in the following JSON format (use key 'pdata' which is an array of slides):
+templates for slides => [
+  T1 = {{
+      "slideNo": ,
+      "templateName": "T1",
+      "heading": "Heading",
+      "subheading": "Sub-Heading",
+      "description": "Description"
+    }}
+  T2 = {{
+      "slideNo": ,
+      "templateName": "T2",
+      "heading": "Heading",
+      "points": [
+        "point 1",
+        "point 2"
+      ],
+      "image":"example/imageurl"
+    }}
+  T3 = {{
+      "slideNo": ,
+      "templateName": "T3",
+      "heading": "Heading",
+      "topic1": "topic 1 name",
+      "points1": [
+        "point 1",
+        "point 2"
+      ],
+      "topic2": "topic 2 name",
+      "points2": [
+        "point 1",
+        "point 2"
+      ]
+    }}
+  T4 = {{
+      "slideNo": ,
+      "templateName": "T4",
+      "heading": "Heading",
+      "topic": "topic",
+      "points": [
+        "point 1",
+        "point 2"
+      ]
+    }}
+  T5 = {{
+      "slideNo": ,
+      "templateName": "T5",
+      "heading": "Heading",
+      "image":"example/imageurl",
+      "imageTitle": "title of image"
+    }}
+  ]
+
+
+Return the output strictly in the following JSON format (use key 'pdata' which is an array of slides), chose the templates from the above templates, use them according to requirements.
 
 {{
   "pdata": [
     {{
-      "slideNo": 1,
-      "templateName": "A1",
+      "slideNo": ,
+      "templateName": "T1",
       "heading": "Heading",
       "subheading": "Sub-Heading",
       "description": "Description"
     }},
     {{
-      "slideNo": 2,
-      "templateName": "B1",
+      "slideNo": ,
+      "templateName": "T2",
       "heading": "Heading",
       "points": [
         "point 1",
         "point 2"
-      ]
+      ],
+      "image":"example/imageurl"
     }}
   ]
 }}
