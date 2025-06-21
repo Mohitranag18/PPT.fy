85689a6ff2de65a28f10fa3c53abaf321978e229
Mohit Rana <mohitr8998@gmail.com>
2025-06-21 15:14:35 +0530

    updated allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['backend.onrender.com', 'localhost', '127.0.0.1', 'https://frontend.vercel.app'] 
+ALLOWED_HOSTS = ['ppt-fy.onrender.com', 'localhost', '127.0.0.1', 'https://ppt-fy.vercel.app'] 
 
 
 # Application definition
